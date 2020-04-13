b25a719676cbb1b43a70a4d0754debe66cd59b0b
Burgos1337 <57829161+Burgos1337@users.noreply.github.com>
2020-04-13 19:49:21 +0300

    500_some_weird_code writeup modified

--- ppc/500_some_weird_code/scripts/some_weird_code.pl
+++ ppc/500_some_weird_code/scripts/some_weird_code.pl
@@ -4,7 +4,7 @@ sub main {
     for (my $i=0;$i<32;++$i) {
         print((grep{/\S/}split(/\W|/,$j))[$i%(split('',$j))[11]+$i%(split('',$j))[25]*$i%19]);
     }
-    print "\n";
+    print("\n");
     return 0;
 }
 main() unless caller;
